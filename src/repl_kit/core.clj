8c3d34394772b7023bddc7142b2fb2125f842f5c
Todd Stout <todd.tstout@gmail.com>
2024-12-21 08:36:44 -0600

    capture almost working pom for publishing

--- src/repl_kit/core.clj
+++ src/repl_kit/core.clj
@@ -1,9 +1,25 @@
 (ns repl-kit.core
   (:require [clojure.tools.cli :refer [parse-opts]]
-            [seesaw.core :refer [pack! show! frame]])
+            [seesaw.core :refer [pack! show! frame]]
+            [seesaw.rsyntax :refer [text-area]]
+            [seesaw.dev :refer [show-events show-options]])
   (:gen-class))
 
 (defn -main [& args] 
-  (-> (frame :title "Hello" :content "Hi there") 
+  (-> (frame :title "Hello" 
+             :content (text-area :syntax :clojure :editable? true)) 
       pack! 
       show!))
+
+
+(comment
+  (seesaw/rtextarea "")
+
+  (seesaw/rtextarea)
+
+  (load-file)
+  (show-events (frame))
+  (show-events (seesaw.rsyntax/text-area :syntax :clojure :editable? true))
+  (show-options (seesaw.rsyntax/text-area :syntax :clojure :editable? true))
+  ;;
+  )
