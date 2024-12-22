cf8b802287fe9662e8654e0f57f68d3d5e18e8e6
Todd Stout <todd.tstout@gmail.com>
2024-12-22 11:08:36 -0600

    Working publish, but missing main manifest

---
+++ src/main/java/repl_kit/core/Empty.java
@@ -0,0 +1,5 @@
+package main.java.repl_kit.core;
+
+public class Empty {
+    
+}
---
+++ src/main/java/repl_kit/core/package-info.java
@@ -0,0 +1,11 @@
+/**
+* Minimal Clojure Editor with REPL
+* <p>
+* More docs will be added here in the future
+* </p>
+*
+* @since 0.0.3
+* @author Todd Stout
+* @version 0.0.3
+*/
+package repl_kit.core;
